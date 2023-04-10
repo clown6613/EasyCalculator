94c67325cdbd30669247f1b10724b7a87f22134c
Kota Yoshikawa <clown.66137@gmail.com>
2023-04-10 19:53:36 +0900

    feat: AC and equal Button

--- app/src/main/java/com/example/easy_caluculator/MainScreen.kt
+++ app/src/main/java/com/example/easy_caluculator/MainScreen.kt
@@ -2,29 +2,73 @@ package com.example.easy_caluculator
 
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
+import androidx.compose.material.ButtonDefaults.buttonColors
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
 @Composable
-fun MainScreen(){
-    Scaffold(modifier = Modifier.fillMaxSize()) {
+fun MainScreen() {
+    Scaffold(modifier = Modifier.padding(20.dp)) {
         Column(modifier = Modifier.padding(it)) {
             Text(
                 text = "a",
-                modifier = Modifier.fillMaxWidth().weight(0.5f)
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .weight(0.5f)
             )
+            Row(modifier = Modifier.fillMaxWidth()) {
+                Button(
+                    onClick = { /*TODO*/ },
+                    modifier = Modifier
+                        .width(80.dp)
+                        .height(50.dp),
+                    colors = buttonColors(backgroundColor = Color.Red)
+                ) {
+                    Text(
+                        text = "AC",
+                        modifier = Modifier,
+                        fontSize = 30.sp,
+                        fontWeight = FontWeight.Bold,
+                        color = Color.White
+                    )
+                }
+                Spacer(modifier = Modifier.width(80.dp))
+                Button(
+                    onClick = { /*TODO*/ },
+                    modifier = Modifier
+                        .width(80.dp)
+                        .height(50.dp)
+                ) {
+                    Text(
+                        text = "=",
+                        modifier = Modifier,
+                        fontSize = 30.sp,
+                        fontWeight = FontWeight.Bold,
+                        textAlign = TextAlign.Justify,
+                        color = Color.White
+                    )
+
+                }
+            }
+
+            Spacer(modifier = Modifier.height(30.dp))
 
             Column(
-                modifier = Modifier.fillMaxSize().weight(0.5f),
+                modifier = Modifier
+                    .fillMaxSize()
+                    .weight(0.5f),
                 verticalArrangement = Arrangement.spacedBy(50.dp)
-            ){
+            ) {
                 Row(
                     modifier = Modifier.fillMaxWidth(),
                     horizontalArrangement = Arrangement.SpaceBetween
@@ -42,7 +86,8 @@ fun MainScreen(){
                         onClick = { /*TODO*/ },
                         modifier = Modifier.size(80.dp)
                     ) {
-                        Text(text = "8",
+                        Text(
+                            text = "8",
                             fontSize = 35.sp
                         )
                     }
@@ -64,7 +109,8 @@ fun MainScreen(){
                         onClick = { /*TODO*/ },
                         modifier = Modifier.size(80.dp)
                     ) {
-                        Text(text = "4",
+                        Text(
+                            text = "4",
                             fontSize = 35.sp
                         )
                     }
@@ -72,7 +118,8 @@ fun MainScreen(){
                         onClick = { /*TODO*/ },
                         modifier = Modifier.size(80.dp)
                     ) {
-                        Text(text = "5",
+                        Text(
+                            text = "5",
                             fontSize = 35.sp
                         )
                     }
@@ -80,7 +127,8 @@ fun MainScreen(){
                         onClick = { /*TODO*/ },
                         modifier = Modifier.size(80.dp)
                     ) {
-                        Text(text = "6",
+                        Text(
+                            text = "6",
                             fontSize = 35.sp
                         )
                     }
@@ -93,7 +141,8 @@ fun MainScreen(){
                         onClick = { /*TODO*/ },
                         modifier = Modifier.size(80.dp)
                     ) {
-                        Text(text = "1",
+                        Text(
+                            text = "1",
                             fontSize = 35.sp
                         )
                     }
@@ -101,7 +150,8 @@ fun MainScreen(){
                         onClick = { /*TODO*/ },
                         modifier = Modifier.size(80.dp)
                     ) {
-                        Text(text = "2",
+                        Text(
+                            text = "2",
                             fontSize = 35.sp
                         )
                     }
@@ -109,7 +159,8 @@ fun MainScreen(){
                         onClick = { /*TODO*/ },
                         modifier = Modifier.size(80.dp)
                     ) {
-                        Text(text = "3",
+                        Text(
+                            text = "3",
                             fontSize = 35.sp
                         )
                     }
@@ -121,6 +172,6 @@ fun MainScreen(){
 
 @Preview
 @Composable
-private fun PreviewMainScreen(){
+private fun PreviewMainScreen() {
     MainScreen()
 }
--- app/src/main/java/com/example/easy_caluculator/MainViewModel.kt
+++ app/src/main/java/com/example/easy_caluculator/MainViewModel.kt
@@ -2,6 +2,4 @@ package com.example.easy_caluculator
 
 import androidx.lifecycle.ViewModel
 
-class MainViewModel : ViewModel() {
-
-}
+class MainViewModel : ViewModel()
